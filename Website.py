09f093435b64798ced1aa14cab1ab3d1dcd5f459
FlxmxBxi <103544436+FlxmxBxi@users.noreply.github.com>
2026-06-13 17:30:49 +0800

    Add files via upload

---
+++ Website.py
@@ -0,0 +1,30 @@
+import streamlit as s
+from backend import chat
+
+s.title("Hi, how can I help You today? ")
+s.write("Calvin is the best programmer")
+with s.sidebar:
+    s.write("Calvin memorials")
+    s.write("He was the best programmer to ever live, handsome tall and cool")
+s.audio_input(label = "Insert message below", sample_rate=16000, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible", width="stretch")
+with s.chat_message(name = "Bot", avatar="🤖", width="stretch"):
+    s.write("How can I help You today")
+input = s.chat_input("Type your message")
+
+if "messages" not in s.session_state:
+    s.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in s.session_state.messages:
+    with s.chat_message(message["role"]):
+        s.markdown(message["content"])
+
+if input:
+    with s.chat_message(name = "user", width="stretch"):
+        s.write(input)
+    s.session_state.messages.append({"role": "user", "content": input})
+
+    with s.chat_message(name = "assistant", width ="stretch"):
+        chatt = s.write_stream(chat(s.session_state.messages)) 
+    s.session_state.messages.append({"role": "user", "content": chatt})
+
---
+++ backend.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="API KEY",
+)
+#secondary code if you have one "API KEY"
+
+
+def chat(history):
+    
+
+    stream = client.chat.completions.create(
+        model="x-ai/grok-build-0.1",
+        messages=history,
+        stream=True,
+    )
+
+    for chunk in stream:
+        content = chunk.choices[0].delta.content
+        if content:
+            yield content
